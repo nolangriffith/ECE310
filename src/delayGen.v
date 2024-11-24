1c01b44a8ee6bb400b9ee7139bdc79e301806cea
nolangriffith <cheesyquesadilla2004@gmail.com>
2024-11-24 17:51:22 -0500

    Update delayGen.v
    add comments

--- src/delayGen.v
+++ src/delayGen.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Nolan Griffith
 // 
-// Create Date: 02/08/2020 08:07:20 PM
+// Create Date: 11/24/2024
 // Design Name: 
 // Module Name: delayGen
 // Project Name: 
@@ -22,15 +22,15 @@
 
 module delayGen(
     input clock,
-    input delayEn,
-    output reg delayDone
+    input delayEn, //enable hi to get the delay
+    output reg delayDone  //the delay has been done (displays handshaking behavior with other features of the zedboard)
     );
     
 reg [17:0] counter;    
 
 always @(posedge clock)
 begin
-    if(delayEn & counter!=200000)
+    if(delayEn & counter!=200000) //increment to make the delay higher through the counter
         counter <= counter+1;
     else
         counter <= 0;
@@ -38,7 +38,7 @@ end
 
 always @(posedge clock)
 begin
-    if(delayEn & counter==200000)
+    if(delayEn & counter==200000)//when it reaches this specific value, then delay is done
         delayDone <= 1'b1;
     else
         delayDone <= 1'b0;
